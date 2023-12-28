0d148ff2bc017c0d73dbc0b3ba6db9abb06e0a9f
bard <wodykjakub@gmail.com>
2023-12-28 14:05:58 +0100

    copied code from Dires

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const Discord = require("discord.js")
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("messageCreate", msg => {
+  console.log(`Received message: ${msg.content}`);
+  if (msg.content.toLowerCase().startsWith("ping")) {
+    console.log("Ping command detected. Replying with pong.");
+    msg.reply("pong");
+  }
+});
+
+
+client.login(process.env.TOKEN)
